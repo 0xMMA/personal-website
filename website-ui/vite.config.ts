8ae325afb1c64fc54d7a9da2742e26e0426fb2e6
Michael Martin <darth.maddin.g1@gmail.com>
2024-03-19 01:44:21 +0000

    add website-backend with .net and azure functions

--- website-ui/vite.config.ts
+++ website-ui/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			// Use polling for file changes
+			usePolling: true,
+			// Interval in milliseconds (optional)
+			interval: 250
+		}
+	}
 });
